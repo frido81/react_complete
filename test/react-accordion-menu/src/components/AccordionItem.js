32a1c61a285632f6ea0b3725198eb6d72c2ade68
Frido Ferdinand <frido.ferdinand@gmail.com>
2024-02-27 14:40:29 +0100

    react accordion

--- test/react-accordion-menu/src/components/AccordionItem.js
+++ test/react-accordion-menu/src/components/AccordionItem.js
@@ -1,12 +1,31 @@
-const AccordionItem = ({ faq, onToggle, active }) => {
+
+import { useState } from "react";
+import { useRef } from "react";
+
+const AccordionItem = ({ faq }) => {
     const { question, answer } = faq;
+    const [clicked, setClicked] = useState(false);
+    const contentEl = useRef();
+    const handleToggle = (index) => {
+        setClicked((prev) => !prev);
+    }
+
     return (
-        <li className={`accordion_item ${active ? "active" : ""}`}>
-            <button className="button" onClick={onToggle}>
+        <li className={`accordion_item ${clicked ? "active" : ""}`}>
+            <button className="button" onClick={handleToggle}>
                 {question}
-                <span className="control">{active ? "—" : "+"} </span>
+                <span className="control">{clicked ? "—" : "+"} </span>
             </button>
-            <div className={`answer_wrapper ${active ? "open" : ""}`}>
+            <div
+                ref={contentEl}
+                className="answer_wrapper"
+                style={
+                    clicked
+                        ? { height: contentEl.current.scrollHeight }
+                        : { height: "0px" }
+                }
+
+            >
                 <div className="answer">{answer}</div>
             </div>
         </li >
--- test/react-accordion-menu/src/components/App.js
+++ test/react-accordion-menu/src/components/App.js
@@ -1,6 +1,9 @@
 import Header from "./Header";
 import Accordion from "./Accordion";
 
+// from:
+// https://blog.logrocket.com/how-to-build-a-react-accordion-menu-from-scratch/
+
 const App = () => {
     return (
         <div className="container">
