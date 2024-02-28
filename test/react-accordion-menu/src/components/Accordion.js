5eb7e212f95e60169a2163b605436a164a00ecc4
Frido Ferdinand <frido.ferdinand@gmail.com>
2024-02-28 14:25:36 +0100

    cleanup

--- test/react-accordion-menu/src/components/Accordion.js
+++ test/react-accordion-menu/src/components/Accordion.js
@@ -1,34 +1,22 @@
 
 
-import { faqs, faqs2 } from "../data";
 import { notes } from "../notes";
 import AccordionItem from "./AccordionItem";
 
 const Accordion = () => {
 
-    // const [clicked, setClicked] = useState("0");
-    // const handleToggle = (index) => {
-    //     if (clicked === index) {
-    //         return setClicked("0");
-    //     }
-    //     setClicked(index);
-    // };
-
-    // for (const notebook in notes) {
-    //     console.log(notebook)
-    // }
-
-    Object.keys(notes).map((note, index) => (console.log(note, index)));
-
+    // use object.keys everywhere to extract arrays from json object
 
     return (
         <ul className="accordion">
-            {Object.keys(notes).map((note, index) => (
-                <AccordionItem
-                    // onToggle={() => handleToggle(index)}
-                    // active={clicked === index}
-                    key={index} note={note} sections={notes} />
-            ))}
+            {
+                Object.keys(notes).map((note, index) => {
+                    const sections = Object.keys(notes[note]);
+                    return (
+                        <AccordionItem
+                            key={index} note={note} sections={sections} />
+                    )
+                })}
         </ul>
     );
 };
--- test/react-accordion-menu/src/components/AccordionItem.js
+++ test/react-accordion-menu/src/components/AccordionItem.js
@@ -3,19 +3,14 @@ import { useState } from "react";
 import { useRef } from "react";
 
 const AccordionItem = (props) => {
-    // const { question, answer1, answer2 } = faq;
+
     const [clicked, setClicked] = useState(false);
+
     const contentEl = useRef();
+
     const handleToggle = (index) => {
         setClicked((prev) => !prev);
     }
-
-    console.log(props.note);
-    console.log(props.sections);
-
-
-    const sections = Object.keys(props.sections[props.note]);
-    console.log(sections);
 
     return (
         <li className={`accordion_item ${clicked ? "active" : ""}`}>
@@ -33,10 +28,7 @@ const AccordionItem = (props) => {
                 }
 
             >
-
-
-
-                {sections.map((section, index) => (
+                {props.sections.map((section, index) => (
                     <div className="answer">
                         {section}
                     </div>
