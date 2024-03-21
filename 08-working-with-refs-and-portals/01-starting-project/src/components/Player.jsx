57a45fc3e3760580c0a533efee6358951efc3219
Frido Ferdinand <frido.ferdinand@gmail.com>
2024-03-21 11:25:29 +0100

    132: Introducing refs, connecting & accesssing HTML elements via refs

--- 08-working-with-refs-and-portals/01-starting-project/src/components/Player.jsx
+++ 08-working-with-refs-and-portals/01-starting-project/src/components/Player.jsx
@@ -1,23 +1,20 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function Player() {
-    const [enteredPlayerName, setEnteredPlayerName] = useState('');
-    const [submitted, setSubmitted] = useState(false);
+    // read input field from
+    const playerName = useRef();
 
-    function handleChange(event) {
-        setSubmitted(false);
-        setEnteredPlayerName(event.target.value);
-    }
+    const [enteredPlayerName, setEnteredPlayerName] = useState('');
 
     function handleClick() {
-        setSubmitted(true);
+        setEnteredPlayerName(playerName.current.value);
     }
 
     return (
         <section id="player">
-            <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
+            <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
             <p>
-                <input type="text" onChange={handleChange} value={enteredPlayerName} />
+                <input type="text" ref={playerName} />
                 <button onClick={handleClick}>Set Name</button>
             </p>
         </section>
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 npm create vite@latest dynamic_styles -- --template react
+
+## Nice
+
+Ternary short form
+
+```jsx
+            <h2>Welcome {enteredPlayerName ? enteredPlyaerName 'unknown entity'}</h2>
+            <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
+```
