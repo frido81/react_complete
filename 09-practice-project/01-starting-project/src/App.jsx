2e69b7dbf0376edede39165d1df7bffc2d55b94e
Frido Ferdinand <frido.ferdinand@gmail.com>
2024-03-27 14:06:13 +0100

    asdf

--- 09-practice-project/01-starting-project/src/App.jsx
+++ 09-practice-project/01-starting-project/src/App.jsx
@@ -40,7 +40,9 @@ function App() {
                     <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">YOUR PROJECTS</h2>
                     <CreateProjectButton onClick={toggleCreateProject} label="+ Add Project" />
                     <ul className="mt-8">
-                        <li className="text-stone-50 mx-4 my-2">inactive</li>
+                        {projects.map((project) => (
+                            <li className="text-stone-50 mx-4 my-2">{project.title}</li>
+                        ))}
                     </ul>
                 </aside>
 
